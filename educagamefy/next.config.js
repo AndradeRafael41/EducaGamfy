3007fa31b150322554f5fee00909e4ac9c98e5c9
Rafael Andrade <rafael.andradev256@gmail.com>
2025-10-05 17:29:05 -0300

    :wrench: set next config for files upload allow

--- educagamefy/next.config.js
+++ educagamefy/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb', 
+    },
+  },
+
+  api: {
+    bodyParser: false,
+  },
 };
 
 module.exports = nextConfig;
